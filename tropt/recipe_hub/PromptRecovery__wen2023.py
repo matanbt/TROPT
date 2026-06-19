cb36fbd12614dc8da3cf71520e7044081d390b90
MatanBT <mtnbt123@gmail.com>
2026-06-20 00:17:01 +0300

    Minor fixes

--- tropt/recipe_hub/PromptRecovery__wen2023.py
+++ tropt/recipe_hub/PromptRecovery__wen2023.py
@@ -79,7 +79,7 @@ def get_image_embedding_for_clip_model(
     # Clean up: we only needed the vision encoder
     del clip_model, processor
 
-    return image_emb.pooler_output
+    return image_emb
 
 
 def prompt_recovery__wen2023(
